dd03ce9c8058083569832b6fb54c49117c06c8b4
이정원 <jung_won@ijeong-won-ui-noteubug.local>
2024-01-03 00:50:46 +0900

    플러터에 보이는 텍스트는 여기에 정리

---
+++ lib/text_widgets.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+Widget email() {
+  return const Text(
+    '이메일 주소 *',
+    style: TextStyle(
+      fontSize: 16,
+    ),
+    textAlign: TextAlign.left,
+  );
+}
+
+Widget password() {
+  return const Text(
+    '비밀번호 *',
+    style: TextStyle(fontSize: 16),
+  );
+}
+
+Widget passwordValidationNotice() {
+  return const Text(
+    '영문 대문자와 소문자, 숫자, 특수문자 중 2가지 이상을 조합하여 6~20자로 입력해주세요.',
+    style: TextStyle(
+      color: Colors.red,
+    ),
+  );
+}
+
+Widget name() {
+  return const Text(
+    '이름*',
+    style: TextStyle(fontSize: 16),
+  );
+}
+
+Widget phoneNumner() {
+  return const Text(
+    '휴대폰 번호',
+    style: TextStyle(fontSize: 16),
+  );
+}
+
+Widget address() {
+  return const Text(
+    '주소',
+    style: TextStyle(fontSize: 16),
+  );
+}
+
+Widget birth() {
+  return const Text(
+    '생년월일',
+    style: TextStyle(fontSize: 16),
+  );
+}
