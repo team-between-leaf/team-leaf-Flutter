59eb8b874492a5dc6c74579f281c3f08873df4eb
이정원 <jung_won@ijeong-won-ui-noteubug.local>
2024-01-09 13:03:06 +0900

    chore: 이메일 필드 벗어나면 유효성 검사

--- lib/join_membership/join_membership_detail/jmd_required_information.dart
+++ lib/join_membership/join_membership_detail/jmd_required_information.dart
@@ -12,18 +12,32 @@ class RequiredInformation extends StatefulWidget {
 }
 
 class _RequiredInformationState extends State<RequiredInformation> {
-  final FocusNode _passwordFocusNode = FocusNode();
-  final FocusNode _passwordcheckFocusNode = FocusNode();
-  final FocusNode _nameFocusNode = FocusNode();
-  final FocusNode _phoneFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode(); // 비밀번호로 이동
+  final FocusNode _passwordcheckFocusNode = FocusNode(); // 비밀번호 확인으로 이동
+  final FocusNode _nameFocusNode = FocusNode(); // 이름으로 이동
+  final FocusNode _phoneFocusNode = FocusNode(); // 핸드폰 번호로 이동
   final _formKey = GlobalKey<FormState>(); // 폼 키 추가
+  final FocusNode _emailcheck = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _emailcheck.addListener(() {
+      if (!_emailcheck.hasFocus) {
+        // 이메일 필드에서 포커스가 벗어났을 때 유효성 검사를 수행
+        _formKey.currentState?.validate();
+      }
+    });
+  }
 
   @override
   void dispose() {
+    // 초기화
     _passwordFocusNode.dispose();
     _passwordcheckFocusNode.dispose();
     _nameFocusNode.dispose();
     _phoneFocusNode.dispose();
+    _emailcheck.dispose();
     super.dispose();
   }
 
@@ -33,31 +47,31 @@ class _RequiredInformationState extends State<RequiredInformation> {
       padding: const EdgeInsets.symmetric(vertical: 10),
       child: Form(
         key: _formKey, // 폼 키 할당
-
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(height: 80),
             email(),
             TextFormField(
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(), hintText: '이메일 주소'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return '이메일 주소를 입력해주세요.';
-                  } else if (!emailRegExp.hasMatch(value)) {
-                    return '유효한 이메일 주소가 아닙니다.';
-                  }
-                  return null; // 입력이 유효할 경우 null 반환
-                },
-                onFieldSubmitted: (value) {
-                  // 유효성 검사 수행
-                  if (_formKey.currentState!.validate()) {
-                    // 유효하면 다음 필드로 포커스 이동
-                    FocusScope.of(context).requestFocus(_passwordFocusNode);
-                  }
-                }),
+              focusNode: _emailcheck,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), hintText: '이메일 주소'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '이메일 주소를 입력해주세요.';
+                } else if (!emailRegExp.hasMatch(value)) {
+                  return '유효한 이메일 주소가 아닙니다.';
+                }
+                return null; // 입력이 유효할 경우 null 반환
+              },
+              onFieldSubmitted: (value) {
+                // 유효성 검사 수행
+                if (_formKey.currentState!.validate()) {
+                  // 유효하면 다음 필드로 포커스 이동
+                  FocusScope.of(context).requestFocus(_passwordFocusNode);
+                }
+              },
+            ),
             const SizedBox(height: 30),
             password(),
             TextFormField(
--- lib/join_membership/join_membership_page.dart
+++ lib/join_membership/join_membership_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:team_between_leaf/join_membership/join_membership_detail/jmd_membership_button.dart';
 import 'package:team_between_leaf/join_membership/join_membership_detail/jmd_required_information.dart';
-import 'package:team_between_leaf/join_membership/join_membership_detail/jmd_selection_information.dart';
 
 class JoinMembershipPage extends StatefulWidget {
   const JoinMembershipPage({super.key});
@@ -24,7 +23,6 @@ class _JoinMembershipPageState extends State<JoinMembershipPage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 RequiredInformation(), //  필수 정보 (이메일 주소, 비밀번호, 이름, 휴대폰 번호)
-                SelectionInformation(), // 선택 정보 (주소, 생년월일)
                 JoinMembershipButton(), // 회원가입 버튼
               ],
             ),
